7689668add9fc5585d4aa0598be27f938362fa42
Lerun4ik <lera.shakhoffa@gmail.com>
2021-03-09 22:00:16 +0300

    create: media rules for hotel section

---
+++ js/mymap.js
@@ -0,0 +1,21 @@
+ ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [7.838839, 98.392374],
+      zoom: 12,
+      controls: [
+        'zoomControl', 
+        'routeButtonControl', 
+        'trafficControl', 
+        'typeSelector', 
+        'fullscreenControl', 
+        ]
+});
+     var myPlacemark = new ymaps.Placemark([7.838839, 98.392374], {
+         hintContent: 'GRAND HILTON HOTEL',
+         balloonContent: 'H'
+});
+  myMap.geoObjects.add(myPlacemark);
+  disable('scrollZoom')
+}
+map.behaviors.disable('drag'); 
