ba4b7e9756e3eb95310470dc7872d407ca722b10
Lerun4ik <lera.shakhoffa@gmail.com>
2021-03-17 12:42:50 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -27,7 +27,14 @@ var reviewsSlider = new Swiper('.reviews-slider', {
     onlyInViewport: false
   },
 });
+
 let newsletter=document.getElementById('newsletter');
 this.addEventListener("scroll",function(){
 newsletter.style["backgroundPositionY"]=100-1913/1842*this.scrollY/1842*100+'%';
-})
+});
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function() {
+console.log("Клик по кнопке");
+document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
+});
