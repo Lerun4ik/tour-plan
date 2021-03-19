5eae2146cc541f0208145223fdd7b702d1b3ff22
Lerun4ik <lera.shakhoffa@gmail.com>
2021-03-19 20:00:30 +0300

    create: modal windows

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -28,13 +29,33 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   },
 });
 
-let newsletter=document.getElementById('newsletter');
-this.addEventListener("scroll",function(){
-newsletter.style["backgroundPositionY"]=100-1913/1842*this.scrollY/1842*100+'%';
-});
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener('click', function() {
 console.log("Клик по кнопке");
 document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
+});
+
+var modalButton = $('[data-toggle=modal]')
+var closeModalButton = $(".modal__close")
+modalButton.on("click", openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+function closeModal(event) {
+  event.preventDefault ();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
+});
+let newsletter=document.getElementById('newsletter');
+this.addEventListener("scroll",function(){
+newsletter.style["backgroundPositionY"]=100-1913/1842*this.scrollY/1842*100+'%';
 });
